aba449e753562d5927f7a7649c4f0a5d541c5c10
Subham Mondal <subhammondal2502@gmail.com>
2026-09-25 19:41:39 +0530

    Display employee name and basic salary .

--- Employee salary and Bonus checker.py
+++ Employee salary and Bonus checker.py
@@ -3,7 +3,8 @@ name=input("enter employee name : ")
 basic_salary=int(input("enter basic salary : "))
 experience=int(input("enter year of experience : "))
 rating=int(input("enter performance rating 1 to 5 : "))
-
+print("Employee name : ", name)
+print("Basic salary : ", basic_salary)
                             
         
     
