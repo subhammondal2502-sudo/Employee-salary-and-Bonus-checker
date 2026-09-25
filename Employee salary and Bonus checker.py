0ebd660702b54a59dfe6ca5d15212fdf790aa8c9
Subham Mondal <subhammondal2502@gmail.com>
2026-09-25 19:35:41 +0530

    Add files via upload

---
+++ Employee salary and Bonus checker.py
@@ -0,0 +1,9 @@
+#Employee salary and Bonus checker......
+name=input("enter employee name : ")
+basic_salary=int(input("enter basic salary : "))
+experience=int(input("enter year of experience : "))
+rating=int(input("enter performance rating 1 to 5 : "))
+
+                            
+        
+    
