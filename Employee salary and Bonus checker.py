2998636ffafbadc5ce5b35faa0ee9ffa76178fee
Subham Mondal <subhammondal2502@gmail.com>
2026-09-26 21:08:50 +0530

    Display final salary for rating 4

--- Employee salary and Bonus checker.py
+++ Employee salary and Bonus checker.py
@@ -30,3 +30,4 @@ else:
                 bonus_amount = basic_salary * (15/ 100)
                 final_salary = basic_salary + bonus_amount
                 print("bonus_amount : ",bonus_amount)
+                print("final salary :",final_salary)
