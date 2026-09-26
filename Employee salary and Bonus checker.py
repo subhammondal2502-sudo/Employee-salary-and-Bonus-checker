f33ffcb6e9307c5d142a815df56a23fe57b90df3
Subham Mondal <subhammondal2502@gmail.com>
2026-09-26 20:46:39 +0530

    Display bonus amount .

--- Employee salary and Bonus checker.py
+++ Employee salary and Bonus checker.py
@@ -16,3 +16,4 @@ else:
         if experience>=5:
             bonus_amount = basic_salary * (20/ 100)
             final_salary = basic_salary + bonus_amount
+            print("bonus_amount : ",bonus_amount)
