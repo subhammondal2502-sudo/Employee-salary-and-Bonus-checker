dbdc960e6776e768a5d8ae8a017827bf362070e8
Subham Mondal <subhammondal2502@gmail.com>
2026-09-26 20:49:47 +0530

    Display bonus percentage .

--- Employee salary and Bonus checker.py
+++ Employee salary and Bonus checker.py
@@ -18,3 +18,4 @@ else:
             final_salary = basic_salary + bonus_amount
             print("bonus_amount : ",bonus_amount)
             print("final salary :",final_salary)
+            print("bonus percentage : ", 20,"%" )
